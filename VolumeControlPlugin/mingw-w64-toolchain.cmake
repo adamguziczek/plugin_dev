48e983dcd6ce78d57d246c6eae47518015162f51
Adam Guziczek <aguziczek2021@my.fit.edu>
2025-05-03 15:54:31 -0400

    fix: enhance MinGW toolchain file to resolve cross-compilation errors

--- VolumeControlPlugin/mingw-w64-toolchain.cmake
+++ VolumeControlPlugin/mingw-w64-toolchain.cmake
@@ -26,10 +26,26 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 # Enable shared libraries by default for VST plugins
 set(BUILD_SHARED_LIBS ON)
 
-# Special flags for MinGW cross-compilation
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static-libgcc -static-libstdc++")
+# Compiler flags to fix cross-compilation issues with JUCE
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++ -fpermissive")
+# Disable specific warnings that cause errors in JUCE/Harfbuzz when cross-compiling
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-shift-count-overflow -Wno-narrowing")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-implicit-fallthrough -Wno-deprecated-declarations")
+
+# Add Windows-specific defines for JUCE
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DWINDOWS=1 -D_WINDOWS=1 -DWIN32=1 -D_WIN32=1")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_WIN64=1 -DWIN64=1 -DJUCE_MINGW=1")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DJUCE_WINDOWS=1 -DJUCE_WASAPI=1 -DJUCE_DIRECTSOUND=1")
+
+# Ensure JUCE uses Windows file paths
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DJUCE_WINDOWS_USE_NATIVE_FILE_PATHS=1")
+
+# Additional linker flags
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -static-libgcc -static-libstdc++ -Wl,-Bstatic,--whole-archive -lpthread -Wl,--no-whole-archive")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -static-libgcc -static-libstdc++ -Wl,-Bstatic,--whole-archive -lpthread -Wl,--no-whole-archive")
+
+# Link with Windows libraries needed by JUCE
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lole32 -loleaut32 -lrpcrt4 -lshlwapi -luuid -lwsock32 -lws2_32 -lwininet -lversion -lwinmm")
 
 # Ensure DLLs are created with proper extension
 set(CMAKE_SHARED_LIBRARY_PREFIX "")
@@ -38,7 +54,15 @@ set(CMAKE_SHARED_LIBRARY_SUFFIX ".dll")
 # Disable certain JUCE-related features that might not work well in cross-compilation
 set(JUCE_BUILD_MISC_UTILITIES OFF CACHE BOOL "")
 set(JUCE_USE_CURL OFF CACHE BOOL "")
+set(JUCE_WEB_BROWSER OFF CACHE BOOL "")
 
-# Define Windows platform
-add_definitions(-D_WIN32)
-add_definitions(-DWINDOWS=1)
+# Optimization level
+set(CMAKE_CXX_FLAGS_RELEASE "-O3")
+
+# Define build type as Release by default
+if(NOT CMAKE_BUILD_TYPE)
+    set(CMAKE_BUILD_TYPE Release CACHE STRING "Build type" FORCE)
+endif()
+
+# Use position independent code
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
